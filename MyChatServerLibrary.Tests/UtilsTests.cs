23d6192fa7460babc8a87ade84232538d46f0ccd
Andriy Fetsyuk <AndreusAnd@gmail.com>
2013-11-13 23:58:41 +0200

    ChatServer: added tests

---
+++ MyChatServerLibrary.Tests/UtilsTests.cs
@@ -0,0 +1,19 @@
+﻿namespace Andriy.MyChat.Server.Tests
+{
+    using System.Net.Sockets;
+
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class UtilsTests
+    {
+        [Test]
+        public void TCPClient2IPAddressTest()
+        {
+            var tcpClient = new TcpClient("google.com", 80);
+            var actual = Utils.TCPClient2IPAddress(tcpClient);
+            tcpClient.Close();
+            Assert.AreEqual("173.194.39.71", actual.MapToIPv4().ToString());
+        }
+    }
+}
