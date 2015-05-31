d1c873f7194bb04b5aa40741b8fef79d8b9ad9c5
Andriy Fetsyuk <AndreusAnd@gmail.com>
2015-05-31 10:13:56 +0300

    Added tests

---
+++ MyChat.Tests/WorkflowTests.cs
@@ -0,0 +1,19 @@
+﻿namespace MyChat.Tests
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class WorkflowTests
+    {
+        [Test]
+        public void Run2Clients()
+        {
+            
+        }
+    }
+}
