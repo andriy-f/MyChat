6a95edb057c9de53d90b197864d52f9e4505e326
Andriy Fetsyuk <AndreusAnd@gmail.com>
2015-07-19 22:02:07 +0300

    Some tests along the way

---
+++ MyChat.Common.Tests/NetworkTests.cs
@@ -0,0 +1,23 @@
+﻿namespace MyChat.Common.Tests
+{
+    using System.Net;
+    using System.Net.Sockets;
+
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class NetworkTests
+    {
+        public static void TestTcpLisener()
+        {
+            var tcpListener = new TcpListener(IPAddress.Any, 3456);
+            tcpListener.Start();
+            tcpListener.AcceptTcpClientAsync();
+
+            var tcpClient = new TcpClient();
+            tcpClient.Connect(IPAddress.Parse("127.0.0.1"), 3456);
+
+            var clnt = tcpListener.AcceptTcpClientAsync().ConfigureAwait(true).GetAwaiter().GetResult();
+        }
+    }
+}
