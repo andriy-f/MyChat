4ef5ce7b0afed077c014c2a33dc2c0d414b450b9
Andriy Fetsyuk <AndreusAnd@gmail.com>
2015-08-02 18:59:47 +0300

    Fixed Common.Utils (UTF8 serialization)

---
+++ MyChat.Common.Tests/UtilsTests.cs
@@ -0,0 +1,36 @@
+﻿namespace MyChat.Common.Tests
+{
+    using NUnit.Framework;
+
+    [TestFixture]
+    class UtilsTests
+    {
+        [Test]
+        public void SerializaDeserializeUtf8StrNormalTest()
+        {
+            const string someStr = "Green red pepper";
+            var serialized = Utils.SerializeUTF8String(someStr);
+            var back = Utils.DeSerializeUTF8String(serialized);
+
+            Assert.AreEqual(someStr, back);
+        }
+
+        [Test]
+        public void SerializaDeserializeUtf8StrNullTest()
+        {
+            var serialized = Utils.SerializeUTF8String(null);
+            var back = Utils.DeSerializeUTF8String(serialized);
+
+            Assert.AreEqual(string.Empty, back);
+        }
+
+        [Test]
+        public void SerializaDeserializeUtf8StrEmptyTest()
+        {
+            var serialized = Utils.SerializeUTF8String(string.Empty);
+            var back = Utils.DeSerializeUTF8String(serialized);
+
+            Assert.AreEqual(string.Empty, back);
+        }
+    }
+}
--- MyChat.Common/Utils.cs
+++ MyChat.Common/Utils.cs
@@ -1,22 +1,43 @@
 ﻿namespace MyChat.Common
 {
     using System;
-    using System.Linq;
     using System.Text;
 
     public static class Utils
     {
+        /// <summary>
+        /// Serializes string
+        /// </summary>
+        /// <remarks>Serializes null and empty string the same</remarks>
+        /// <param name="s">String to serialize</param>
+        /// <returns>UTF-8 encoded string with length prefix</returns>
         public static byte[] SerializeUTF8String(string s)
         {
-            var utf8Bytes = Encoding.UTF8.GetBytes(s);
-            return BitConverter.GetBytes(utf8Bytes.Length).Union(utf8Bytes).ToArray();
+            var str = s ?? string.Empty;
+            var utf8EncodedBytes = Encoding.UTF8.GetBytes(str);
+            var lenPrefixBytes = BitConverter.GetBytes(utf8EncodedBytes.Length);
+            var ret = new byte[lenPrefixBytes.Length + utf8EncodedBytes.Length];
+            Buffer.BlockCopy(lenPrefixBytes, 0, ret, 0, lenPrefixBytes.Length);
+            Buffer.BlockCopy(utf8EncodedBytes, 0, ret, lenPrefixBytes.Length, utf8EncodedBytes.Length);
+            return ret;
         }
 
+        /// <summary>
+        /// Deserializes string
+        /// </summary>
+        /// <param name="data"></param>
+        /// <returns></returns>
         public static string DeSerializeUTF8String(byte[] data)
         {
             return DeSerializeUTF8String(data, 0);
         }
 
+        /// <summary>
+        /// Deserializes string
+        /// </summary>
+        /// <param name="data">UTF-8 encoded string with length prefix</param>
+        /// <param name="offset">offset in byte array</param>
+        /// <returns></returns>
         public static string DeSerializeUTF8String(byte[] data, int offset)
         {
             var serializedStrLen = BitConverter.ToInt32(data, offset);
